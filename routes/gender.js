206fcfb231d72eb6d69f7e17ad2a744ebf04f142
vncnklv <vncnklv0@gmail.com>
2021-07-13 11:02:39 +0300

    remake the querie with async/await syntax

--- routes/gender.js
+++ routes/gender.js
@@ -1,16 +1,13 @@
 const _ = require('underscore');
-// const mongoUtil = require('../dbconnection');
 
-// const db = mongoUtil.getDb();
-
-module.exports = function routeGender(req, res) {
+module.exports = async function routeGender(req, res) {
   const { params } = req;
   const { db } = req.app.locals;
 
-  db.collection('categories').find({ id: params.gender }).toArray((collErr, items) => {
-    res.render('gender', {
-      _,
-      items,
-    });
+  const items = await db.collection('categories').find({ id: params.gender }).toArray();
+
+  res.render('gender', {
+    _,
+    items,
   });
 };
