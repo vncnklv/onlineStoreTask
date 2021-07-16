c16280e273972b95a941f539cccfbc46d99be640
vncnklv <vncnklv0@gmail.com>
2021-07-16 16:36:26 +0300

    creted soap request for price of usd in leu

--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,7 @@
 const _ = require('underscore');
+const soap = require('soap');
+const { result } = require('underscore');
+const url = 'http://infovalutar.ro/curs.asmx?wsdl';
 
 module.exports = async function routeCategory(req, res) {
     const { params } = req;
@@ -6,10 +9,21 @@ module.exports = async function routeCategory(req, res) {
     const { db } = req.app.locals;
 
     const product = await db.collection('products').findOne({ id: params.id });
-
-    //Validation
     if (product == null) res.status(404).send('Not found');
-    console.log(product == null);
+
+    // sync
+
+    soap.createClient(url, (err, client) => {
+        client.getlatestvalue({ Moneda: 'USD' }, (err, result) => {
+            console.log(result);
+        });
+    });
+
+    // getlatestvalue is not async function, so this cant happen async
+
+    // const client = await soap.createClientAsync(url);
+    // const currency = await client.getlatestvalue({ Moneda: 'EUR' });
+    // console.log(currency);
 
     res.render('product', {
         _,
