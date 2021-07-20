2986ad132f5e6bd0739f7eed1801222315d75ee1
vncnklv <vncnklv0@gmail.com>
2021-07-20 17:48:29 +0300

    header and part of pdp styles

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,22 @@
+$(document).ready(function () {
+    if (window.innerWidth < 992) {
+        $('.navbar').addClass('bg-dark navbar-dark');
+        $('#navbar_wrapper')
+            .addClass('container-fluid bg-dark')
+            .removeClass('container');
+    }
+});
 
+$(window).resize(function () {
+    if (window.innerWidth < 992) {
+        $('.navbar').addClass('bg-dark navbar-dark');
+        $('#navbar_wrapper')
+            .addClass('container-fluid bg-dark')
+            .removeClass('container');
+    } else {
+        $('.navbar').removeClass('bg-dark navbar-dark');
+        $('#navbar_wrapper')
+            .removeClass('container-fluid bg-dark')
+            .addClass('container');
+    }
+});
--- public/scripts/product.js
+++ public/scripts/product.js
@@ -1,17 +1,25 @@
-const currencySelector = document.getElementById('currency');
-const priceSpan = document.getElementById('price');
+// const currencySelector = document.getElementById('currency');
+// const priceSpan = document.getElementById('price');
 
-currencySelector.addEventListener('change', () => {
-    const currency = currencySelector.value;
+// currencySelector.addEventListener('change', () => {
+//     const currency = currencySelector.value;
 
-    switch (currency) {
-        case 'usd':
-            priceSpan.innerHTML = prodPriceInUsd;
-            break;
-        case 'leu':
-            let priceInLeu = prodPriceInUsd * priceUsdToLeu;
-            priceInLeu = Math.round((priceInLeu + Number.EPSILON) * 100) / 100;
-            priceSpan.innerHTML = priceInLeu.toFixed(2);
-            break;
-    }
+//     switch (currency) {
+//         case 'usd':
+//             priceSpan.innerHTML = prodPriceInUsd;
+//             break;
+//         case 'leu':
+//             let priceInLeu = prodPriceInUsd * priceUsdToLeu;
+//             priceInLeu = Math.round((priceInLeu + Number.EPSILON) * 100) / 100;
+//             priceSpan.innerHTML = priceInLeu.toFixed(2);
+//             break;
+//     }
+// });
+
+const $sizeMenuValues = $('#size-menu').children();
+
+$sizeMenuValues.on('click', (e) => {
+    $('.size-menu-active').removeClass('size-menu-active');
+    const $target = e.target;
+    $($target).addClass('size-menu-active');
 });
